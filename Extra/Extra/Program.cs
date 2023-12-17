a9cd09fe07fa26ac574f1fcd871028ba3bfb840a
Cissevos <r0938854@student.thomasmore.be>
2023-12-17 22:34:12 +0100

    case study

---
+++ Extra/Extra/Program.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using CsvHelper;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a football team (in English): ");
+        string? searchTerm = Console.ReadLine();
+
+        // Configuring the Chrome driver
+        var chromeOptions = new ChromeOptions();
+        //chromeOptions.AddArgument("--headless"); // Optional: make the browser invisible
+        IWebDriver driver = new ChromeDriver(chromeOptions);
+
+        // Navigate to whoscored.com search page
+        driver.Navigate().GoToUrl($"https://www.whoscored.com/Search/?t={searchTerm}");
+
+        // Click the first team link in the search results
+        var teamLink = driver.FindElement(By.CssSelector("div.search-result a"));
+        teamLink.Click();
+
+        // Locate the 'Team Statistics' link by its CSS selector
+        IWebElement teamStatisticsLink = driver.FindElement(By.CssSelector("ul li a[href*='/Statistics/'][class='']"));
+
+        // Check if the link is not selected
+        if (teamStatisticsLink != null && !teamStatisticsLink.GetAttribute("class").Contains("selected"))
+        {
+            // Click on the 'Team Statistics' link
+            teamStatisticsLink.Click();
+        }
+
+        // Wait for the page to load
+        var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        wait.Until(d => d.FindElement(By.CssSelector("table#team-goals-grid tbody tr")));
+
+        // Find the goal type and amount from the table
+        var goalRows = driver.FindElements(By.CssSelector("table#team-goals-grid tbody tr"));
+
+        // Create a list to store the scraped goal data
+        List<GoalData> goalDataList = new List<GoalData>();
+
+        // Loop through each row and retrieve goal data
+        foreach (var goalRow in goalRows)
+        {
+            string goalType = goalRow.FindElement(By.CssSelector(".situation-type")).Text;
+            int goalAmount = int.Parse(goalRow.FindElement(By.CssSelector(".situation-value .stat-value")).Text);
+
+            // Add the scraped goal data to the list
+            goalDataList.Add(new GoalData { GoalType = goalType, GoalAmount = goalAmount });
+
+            Console.WriteLine($"Goal Type: {goalType}, Goals: {goalAmount}");
+        }
+
+        // Write the goal data to CSV file
+        using (var writer = new StreamWriter("whoscored.csv"))
+        using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(goalDataList);
+        }
+
+        // Write the goal data to JSON file
+        File.WriteAllText("whoscored.json", JsonConvert.SerializeObject(goalDataList, Formatting.Indented));
+
+        // Close the browser
+        driver.Quit();
+    }
+}
+
+// Define a class to represent the goal data
+public class GoalData
+{
+    public string? GoalType { get; set; }
+    public int GoalAmount { get; set; }
+}
---
+++ Jobs/Jobs/Program.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using CsvHelper;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Voer een zoekterm in: ");
+        string? zoekterm = Console.ReadLine();
+
+        // Configuratie van de Chrome-driver
+        var chromeOptions = new ChromeOptions();
+        //chromeOptions.AddArgument("--headless"); // Optioneel: maak de browser onzichtbaar
+        IWebDriver driver = new ChromeDriver(chromeOptions);
+
+        // Navigeer naar de ICTJob-website met de opgegeven zoekterm
+        driver.Navigate().GoToUrl($"https://www.ictjob.be/nl/it-vacatures-zoeken?keywords={zoekterm}");
+        var datumKnop = driver.FindElement(By.CssSelector("a#sort-by-date"));
+        datumKnop.Click(); // Klik op de "datum" knop
+
+        // Wacht tot de pagina is geladen (kan worden verbeterd met expliciete wachttijden of andere methoden)
+        System.Threading.Thread.Sleep(5000);
+
+        // Selecteer de lijst met vacatures
+        var jobList = driver.FindElements(By.CssSelector("div#search-result ul.search-result-list li.clearfix"));
+
+        // Maak een lijst om de geschaafde gegevens op te slaan
+        List<JobData> jobDataList = new List<JobData>();
+
+        // Loop door de eerste 5 vacatures en haal de gewenste gegevens op
+        for (int i = 0; i < Math.Min(jobList.Count, 5); i++)
+        {
+            var job = jobList[i];
+            string title = job.FindElement(By.CssSelector("h2")).Text;
+            string company = job.FindElement(By.CssSelector("span.job-company")).Text;
+            string location = job.FindElement(By.CssSelector("span.job-location")).Text;
+            string keywords = job.FindElement(By.CssSelector("span.job-keywords")).Text;
+            string link = job.FindElement(By.CssSelector("a.search-item-link")).GetAttribute("href");
+
+            // Voeg de geschaafde gegevens toe aan de lijst
+            jobDataList.Add(new JobData { Title = title, Company = company, Location = location, Keywords = keywords, Link = link });
+
+            Console.WriteLine($"Job {i + 1}:");
+            Console.WriteLine($"Titel: {title}");
+            Console.WriteLine($"Bedrijf: {company}");
+            Console.WriteLine($"Locatie: {location}");
+            Console.WriteLine($"Keywords: {keywords}");
+            Console.WriteLine($"Link: {link}");
+            Console.WriteLine();
+        }
+
+        // Write the data to CSV file
+        using (var writer = new StreamWriter("jobs.csv"))
+        using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(jobDataList);
+        }
+
+        // Write the data to JSON file
+        File.WriteAllText("jobs.json", JsonConvert.SerializeObject(jobDataList, Formatting.Indented));
+
+        // Sluit de browser
+        driver.Quit();
+    }
+}
+
+// Definieer een klasse om de vacaturegegevens voor elke baan op te slaan
+public class JobData
+{
+    public string? Title { get; set; }
+    public string? Company { get; set; }
+    public string? Location { get; set; }
+    public string? Keywords { get; set; }
+    public string Link { get; set; }
+}
